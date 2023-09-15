c3d5bd311ffd8912a5b013f3aa29975c4750d45e
Devin-Wong <wangjin.devin@gmail.com>
2023-09-15 15:09:39 -0400

    U

--- Topic 2-Function/Code/practice-1.py
+++ Topic 2-Function/Code/practice-1.py
@@ -1,7 +1,7 @@
 ''' to calculate the area of a circle. The formula is pi*r^2 (let pi=3.14), where r denotes radius. 
 
-(1) create a function named `square`, which returns the square if inputing a value. 
-(2) create a function named `area`, which returns the circle area if input radius. In this function, you call `square` function.
+(1) create a function named `square`, which returns the square if inputting a value. 
+(2) create a function named `area`, which returns the circle area if inputting radius. In this function, you call `square` function.
 (3) print sentence like "The area of a circle with redius 1 is 3.14.".
 (4) use `main()` function to orgnize your program.
 '''
@@ -11,7 +11,6 @@ def main():
     a = area(radius)
     print(f"The area of a circle with redius {radius} is {a}.")
 
-    
 def square(a):
     s = a**2
     return s
